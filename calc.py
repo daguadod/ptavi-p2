ac2a04e6308465d9e41384c6a65da29659a13a82
David Aguado Dianez <daguadod@gamma29.aulas.gsyc.urjc.es>
2017-10-02 10:44:35 +0200

    Pasado pep8 a calc.py

--- calc.py
+++ calc.py
@@ -14,17 +14,12 @@ def minus(op1, op2):
     return op1 - op2
 
 if __name__ == "__main__":
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+
+    operando1, operando2 = int(sys.argv[1]), int(sys.argv[3])
 
     if sys.argv[2] == "suma":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
 
     print(result)
