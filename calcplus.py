dde7988ac17881842b6540c267f5c9d00915a33e
David Aguado Dianez <daguadod@gamma29.aulas.gsyc.urjc.es>
2017-10-02 09:58:26 +0200

    últimos detalles calcplus y cambio de int a float

--- calcplus.py
+++ calcplus.py
@@ -3,25 +3,27 @@
 import sys
 import csv
 
+#Abrimos el fichero .csv y declaramos operador y operando1
 with open(sys.argv[1], newline = '') as f:
     reader = csv.reader(f,delimiter = ',')
     for operacion in reader:
         operador = operacion[0]
-        operando1 = int(operacion[1])
+        operando1 = float(operacion[1])
 
+#Declaramos las operaciones con sus respectivas excepciones
         if operador == 'suma':
             for operando in operacion[2:]:
-                operando1 += int(operando)
+                operando1 += float(operando)
         elif operador == 'resta':
             for operando in operacion[2:]:
-                operando1 -= int(operando)
-        elif operador == 'multiplicacion':
+                operando1 -= float(operando)
+        elif operador == 'multiplica':
             for operando in operacion[2:]:
-                operando1 *= int(operando)
-        elif operador == 'division':
+                operando1 *= float(operando)
+        elif operador == 'divide':
             try:
                 for operando in operacion[2:]:
-                    operando1 /= int(operando)
+                    operando1 /= float(operando)
             except ZeroDivisionError:
                 sys.exit("Division by zero is not allowed")
         else:
