b225e5c3cbc43bf8f0c712058c3b763c2fb9f41d
David Aguado Dianez <daguadod@gamma29.aulas.gsyc.urjc.es>
2017-10-02 09:57:42 +0200

    últimos detalles calcoohija.py

--- calcoohija.py
+++ calcoohija.py
@@ -3,19 +3,19 @@
 import calcoo
 import sys
 
-
+#Declaramos que la clase viene de una clase madre "Calculadora"
 class CalculadoraHija(calcoo.Calculadora):
 
+#Operaciones de multiplicación y división 
     def multi(self):
         return self.valor1 * self.valor2
     
     def divi(self):
         return self.valor1 / self.valor2
 
-
 if __name__ == "__main__":
     try:
-        objetohijo = CalculadoraHija(int(sys.argv[1]), int(sys.argv[3]))
+        objetohijo = CalculadoraHija(float(sys.argv[1]), float(sys.argv[3]))
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -23,9 +23,9 @@ if __name__ == "__main__":
        result = objetohijo.suma()
     elif sys.argv[2] == 'resta':
        result = objetohijo.resta()
-    elif sys.argv[2] == 'multiplicacion':
-        result = objetohijo.multi()
-    elif sys.argv[2] == 'division':
+    elif sys.argv[2] == 'multiplica':
+       result = objetohijo.multi()
+    elif sys.argv[2] == 'divide':
         try:
             result = objetohijo.divi()
         except ZeroDivisionError:
